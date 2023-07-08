02ff3a1c071e165d26952f4011e2fef34a70f650
Karol Programuje <karol341654@gmail.com>
2023-07-08 18:13:19 +0200

    Adding h2 database.

--- TodoApp/src/main/java/kwgh0st/springframework/TodoApp/SpringSecurityConfiguration.java
+++ TodoApp/src/main/java/kwgh0st/springframework/TodoApp/SpringSecurityConfiguration.java
@@ -2,12 +2,18 @@ package kwgh0st.springframework.TodoApp;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
+import java.net.http.HttpRequest;
 import java.util.function.Function;
 
 @Configuration
@@ -34,5 +40,17 @@ public class SpringSecurityConfiguration {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(
+                auth -> auth.anyRequest().authenticated());
+        http.formLogin(Customizer.withDefaults());
+        http.csrf(AbstractHttpConfigurer::disable);
+        http.headers(header -> header.frameOptions(HeadersConfigurer.FrameOptionsConfig::disable));
+
+        return http.build();
+
     }
 }
